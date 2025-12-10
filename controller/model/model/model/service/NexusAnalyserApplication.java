2380322f1b750b2333c78b174223ba417c02ea05
larissaferreiradc3-eng <larissaferreira.dc3@gmail.com>
2025-12-10 08:58:45 -0300

    Create NexusAnalyserApplication.java

---
+++ controller/model/model/model/service/NexusAnalyserApplication.java
@@ -0,0 +1,12 @@
+// NexusAnalyserApplication.java - Em: backend/src/main/java/com/nexus/analyser/
+package com.nexus.analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusAnalyserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NexusAnalyserApplication.class, args);
+    }
+}
