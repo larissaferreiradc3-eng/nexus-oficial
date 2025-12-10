c5704e3263d345e2a368297186c93a31bb2cc70e
larissaferreiradc3-eng <larissaferreira.dc3@gmail.com>
2025-12-10 07:59:57 -0300

    Create nexusanalyserapplication.java

---
+++ nexusanalyserapplication.java
@@ -0,0 +1,15 @@
+// NexusAnalyserApplication.java
+package com.nexus.analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusAnalyserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusAnalyserApplication.class, args);
+    }
+    
+    // Configurações de segurança e CORS iriam aqui.
+}
